7a6a78666e42891d3e47e2d3e81792bc1c359804
Ramon Gonzalez Fernandez (M1) <git@damastacoda.dev>
2025-10-21 15:13:07 -0400

    feature(add testing to auth routes, change pw hash)

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,7 +1,7 @@
 import { Router } from "jsr:@oak/oak";
 
 import { Database } from "@db/sqlite";
-import { createHash } from "node:crypto";
+import { pbkdf2Sync } from "node:crypto";
 import { generateSessionToken } from "../utils/sessionkey.ts";
 
 export function createAuthRouter(db: Database) {
@@ -13,7 +13,6 @@ export function createAuthRouter(db: Database) {
   });
 
   router.post("/login", async (ctx) => {
-
     if (ctx.request.body.type() !== "json") {
       ctx.response.body = { error: "INVALID_REQUEST" };
       ctx.response.status = 400;
@@ -22,24 +21,41 @@ export function createAuthRouter(db: Database) {
 
     const body = await ctx.request.body.json();
 
-    if (typeof body.username !== "string" || typeof body.password !== "string") {
+    if (
+      typeof body.username !== "string" ||
+      typeof body.password !== "string"
+    ) {
       ctx.response.body = { error: "INVALID_REQUEST" };
       ctx.response.status = 400;
       return;
     }
 
-    const salt = db.sql`SELECT salt FROM Users WHERE username = ${body.username};`[0]?.salt;
+    const salt = db
+      .sql`SELECT salt FROM Users WHERE username = ${body.username};`[0]
+      ?.salt;
 
-    if (!salt) { // Get their salt, and check if the user exists.
+    if (!salt) {
+      // Get their salt, and check if the user exists.
       ctx.response.body = { error: "INVALID_CREDENTIALS" };
       ctx.response.status = 403;
       return;
     }
 
-    const hashedPassword = createHash("sha256").update(body.password + salt).digest("hex");
+    const hashedPassword = pbkdf2Sync(
+      body.password,
+      salt,
+      100000,
+      64,
+      "sha256",
+    ).toString("hex");
 
-    const user = db.sql`SELECT username FROM Users WHERE username = ${body.username} AND hash = ${hashedPassword};`[0]?.username;
-    if (!user) { // Check if the password is correct.
+    const user = db
+      .sql`SELECT username FROM Users WHERE username = ${body.username} AND hash = ${hashedPassword};`[
+        0
+      ]
+      ?.username;
+    if (!user) {
+      // Check if the password is correct.
       ctx.response.body = { error: "INVALID_CREDENTIALS" };
       ctx.response.status = 403;
       return;
@@ -48,13 +64,17 @@ export function createAuthRouter(db: Database) {
     const newSessionToken = generateSessionToken();
 
     db.sql`INSERT INTO Sessions (username, token) VALUES (${user}, ${newSessionToken});`;
-    ctx.cookies.set("SESSION", newSessionToken, { httpOnly: true });
+    ctx.cookies.set("SESSION", newSessionToken, {
+      httpOnly: true,
+      maxAge: 48 * 60 * 60,
+      path: "/",
+    });
     ctx.response.status = 200;
   });
 
   router.post("/logout", async (ctx) => {
     const SESSION = await ctx.cookies.get("SESSION");
-    if(!SESSION) {
+    if (!SESSION) {
       ctx.response.status = 200;
       return;
     }
@@ -64,7 +84,6 @@ export function createAuthRouter(db: Database) {
   });
 
   router.post("/register", async (ctx) => {
-
     if (ctx.request.body.type() !== "json") {
       ctx.response.body = { error: "INVALID_REQUEST" };
       ctx.response.status = 400;
@@ -73,16 +92,32 @@ export function createAuthRouter(db: Database) {
 
     const body = await ctx.request.body.json();
 
-    if (typeof body.username !== "string" || typeof body.password !== "string" || typeof body.email !== "string") {
+    if (
+      typeof body.username !== "string" ||
+      typeof body.password !== "string" ||
+      typeof body.email !== "string"
+    ) {
       ctx.response.body = { error: "INVALID_REQUEST" };
       ctx.response.status = 400;
       return;
     }
 
-    const salt = crypto.getRandomValues(new Uint8Array(10)).reduce((acc, byte) => acc + ('0' + byte.toString(16)).slice(-2), '');
-    const hashedPassword = createHash("sha256").update(body.password + salt).digest("hex");
+    const salt = crypto
+      .getRandomValues(new Uint8Array(16))
+      .reduce((acc, byte) => acc + byte.toString(16).padStart(2, "0"), "");
 
-    if(db.sql`SELECT username FROM Users WHERE username = ${body.username};`[0]?.username) {
+    const hashedPassword = pbkdf2Sync(
+      body.password,
+      salt,
+      100000,
+      64,
+      "sha256",
+    ).toString("hex");
+
+    if (
+      db.sql`SELECT username FROM Users WHERE username = ${body.username};`[0]
+        ?.username
+    ) {
       ctx.response.body = { error: "USERNAME_TAKEN" };
       ctx.response.status = 409;
       return;
@@ -91,7 +126,6 @@ export function createAuthRouter(db: Database) {
     db.sql`INSERT INTO Users (username, email, hash, salt) VALUES (${body.username}, ${body.email}, ${hashedPassword}, ${salt});`;
     ctx.response.status = 201;
   });
-    
 
   return router;
 }
