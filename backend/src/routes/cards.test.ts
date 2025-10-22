ba3405ec7d000c2de8909e30870fa5395190349e
Ramon Gonzalez Fernandez (M1) <git@damastacoda.dev>
2025-10-22 16:38:21 -0400

    fix(fix formatting)

--- backend/src/routes/cards.test.ts
+++ backend/src/routes/cards.test.ts
@@ -46,7 +46,9 @@ Deno.test({
     db.sql`INSERT INTO Users (username, email, hash, salt) VALUES (${"testuser"}, ${"testemail@service.webemail"}, ${"c297e57206c7aee60fe2ede4bee13021542d0d472fa690c76557cdccf8610cc6cc63ff0d6f6a2f6433c577c5326d3023aabdedd04e453b43bfe1fd1ccc9cb728"}, ${"salt"})`;
     db.sql`INSERT INTO Sets (id, owner, title) VALUES (${"1111111111111111"}, ${"testuser"}, ${"Test Set"})`;
     db.sql`INSERT INTO Cards (id, set_id, front, back) VALUES (${"1234123412341234"}, ${"1111111111111111"}, ${"Front"}, ${"Back"})`;
-    db.sql`INSERT INTO Sessions (username, token, expires) VALUES (${"testuser"}, ${"token"}, ${Date.now() + 60 * 60 * 24})`;
+    db.sql`INSERT INTO Sessions (username, token, expires) VALUES (${"testuser"}, ${"token"}, ${
+      Date.now() + 60 * 60 * 24
+    })`;
     db.sql`INSERT INTO CardProgress (username, card_id, points, last_reviewed) VALUES (${"testuser"}, ${"1234123412341234"}, ${36}, ${12341234})`;
 
     const progressCtx = testing.createMockContext({
